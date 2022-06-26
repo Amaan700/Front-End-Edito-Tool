8d844815711efdb08ebd448449dc4f249d81e37f
Amaan <md.aman26@gmail.com>
2022-06-26 20:02:03 +0530

    Updates

---
+++ precache-manifest.41fbca832699c283e9fea4d49ff560c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46cae1691d4abb425b8b88d0186cf2a",
+    "url": "/Front-End-Editor-Tool/index.html"
+  },
+  {
+    "revision": "ae817ee93d09ca29183d",
+    "url": "/Front-End-Editor-Tool/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "420066514b823ac1a930",
+    "url": "/Front-End-Editor-Tool/static/css/main.7d2118c4.chunk.css"
+  },
+  {
+    "revision": "ae817ee93d09ca29183d",
+    "url": "/Front-End-Editor-Tool/static/js/2.645c2a15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Front-End-Editor-Tool/static/js/2.645c2a15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "420066514b823ac1a930",
+    "url": "/Front-End-Editor-Tool/static/js/main.f9038633.chunk.js"
+  },
+  {
+    "revision": "7d700b20f34d9cbe1479",
+    "url": "/Front-End-Editor-Tool/static/js/runtime-main.7ce2b6d4.js"
+  }
+]);
